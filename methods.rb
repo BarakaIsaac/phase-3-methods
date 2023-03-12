53efa37dc4b7595e802db8c38d3a903675c79ec8
BarakaIsaac <isaac.mwandau2@gmail.com>
2023-03-12 19:25:00 +0300

    Completed Assignments

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add (num1, num2)
+    num1 + num2
+end
+
+def halve (num)
+    if num.is_a?(Integer)
+        num /2
+    else 
+        nil
+    end
+end
+
+greet_programmer
+greet("Naureen")
+greet("Jimmy")
+greet_with_default
+greet_with_default("Naureen")
+add(2, 5)
+
+
